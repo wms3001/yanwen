c66b46f243300da6e33fd159a72e9525b5af7daa
wms3001 <wms3001@163.com>
2023-03-29 22:21:12 +0800

    添加日志格式化打印

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/wms3001/yanwen
 
 go 1.18
 
+require github.com/golang-module/dongle v0.2.8
+
 require (
 	github.com/emmansun/gmsm v0.16.0 // indirect
-	github.com/golang-module/dongle v0.2.8 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
--- yanwen_test.go
+++ yanwen_test.go
@@ -3,19 +3,26 @@ package yanwen
 import (
 	"log"
 	"testing"
+	"time"
 )
 
+var yanwen = YanWen{
+	Url:       "https://ejf-fat.yw56.com.cn/",
+	UserId:    "100000",
+	Format:    "json",
+	Method:    "",
+	Timestamp: 0,
+	Version:   "V1.0",
+	ApiToken:  "D6140AA383FD8515B09028C586493DDB",
+	Data:      "",
+}
+
 func TestYanWen_Sign(t *testing.T) {
-	yanwen := YanWen{
-		Url:       "",
-		UserId:    "",
-		Format:    "",
-		Method:    "",
-		Timestamp: 0,
-		Version:   "",
-		ApiToken:  "",
-		Data:      "",
-	}
+	yanwen.Method = "express.channel.getlist"
+	yanwen.Timestamp = time.Now().UnixMicro()
 	sign := yanwen.Sign()
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile | log.Lmsgprefix)
+	log.SetPrefix("Message:")
+	log.Println(time.Now().UnixMilli())
 	log.Println(sign)
 }
